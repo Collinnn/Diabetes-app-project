108432d2b1e0e20a7d9ac5cded3942697aa3a3b2
Collinnn <tobias3060@gmail.com>
2022-03-06 22:22:42 +0100

    Not working comit

--- Diabetes-app-project/src/App.vue
+++ Diabetes-app-project/src/App.vue
@@ -1,20 +1,38 @@
 <template>
-  <img alt="Kitty" src="./assets/cat.png">
-  <span>"Big meow"</span>
+    <div id="app">
+        <div id="nav">
+            <router-link v-if="authenticated" to="/login" v-on:click="logout()" replace>Logout</router-link>
+        </div>
+        <router-view @authenticated="setAuthenticated" />
+    </div>
 </template>
 
 <script>
-
-
+    export default {
+        name: 'App',
+        data() {
+            return {
+                authenticated: false
+            }
+        },
+        mounted() {
+            if(!this.authenticated) {
+                this.$router.replace({ name: "login" });
+            }
+        },
+        methods: {
+            setAuthenticated(status) {
+                this.authenticated = status;
+            },
+            logout() {
+                this.authenticated = false;
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+        background-color: #fdfdfd;
+    }
 </style>
---
+++ Diabetes-app-project/src/views/login.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="login-comp">
+        <h1>Login</h1>
+        <input type="text" name="username" v-model="input.username" placerholder="Username" />
+        <input type="password" name="password" v-model="input.password" palceholder="Password" />
+        <button type="button" v-on:click="login()">Login</button>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                input: {
+                    username: "",
+                    password: ""
+                }
+            }
+        },
+        methods: {
+            login() {
+                if(this.input.username != "" && this.input.password != "") {
+                    if(this.input.username =="username" && this.input.password == "password") {
+                        this.$router.replace({ name: "DashBoard"});
+                    } else {
+                        console.log("The username and / or password is incorrect");
+                    }
+                } else {
+                    console.log("Username and/or password was empty");
+                }
+            }
+        }
+    }
+
+</script>
+<style scoped>
+
+
+</style>
