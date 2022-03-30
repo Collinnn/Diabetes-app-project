8e20d2859f2afde709ab9d4423959e815326e9a9
Zwinge <zwingechristopher@hotmail.com>
2022-03-30 07:52:56 +0200

    Added icons

--- Diabetes-app-project/src/App.vue
+++ Diabetes-app-project/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <Topbar />
+  <div id="appLayout" v-if="$route.name !== 'login'">
+    <Topbar />
+  </div>
+  
   <div id="app">
     <router-link to='/'></router-link>
     <router-view  />
--- Diabetes-app-project/src/components/Topbar.vue
+++ Diabetes-app-project/src/components/Topbar.vue
@@ -1,17 +1,50 @@
 <template>
-    <div id="topbar">
-        <h2>TOPBAR</h2> 
-        <button id="settings">Settings</button>
+    <div class ="topbar">
+        <button class = "topbarButton" id = "homeButton" @click="goToPage(dashBoard)">
+            <span class="icon" id="homeIcon"></span>
+        </button>
+        <button class = "topbarButton" id = "userButton" @click="goToPage(profilePage)">
+            <span class="icon" id="userIcon"></span>
+        </button>
+        <button class = "topbarButton" id = "settingsButton">
+            <span class="icon" id="settingsIcon"></span>
+        </button>
+        <div class="sub-menu">
+                <div v-for= "(setting,i) in settings"  :key="i" class="menu-item">
+                    {{ setting.title}}
+                </div>
+            </div>
     </div>
 </template>
 
 <script>
 
-export default {
-    name: "TopbarMenu",
-    data() {
+    import router from "@/router"
+
+    export default {
+        name: "TopbarMenu",
+        data() {
             return {
-                title: 'Sidebar'
+                title: 'Sidebar',
+                settings: [
+                    {
+                        title: 'Interface settings', 
+                        link: '#'
+                    },
+                    {
+                        title: 'Darkmode', 
+                        link: '#'
+                    },
+                    {
+                        title: 'Advanced settings', 
+                        link: '#'
+                    }
+                ]
+            }
+        },
+        methods: {
+            goToPage(pageName) {
+                router.push({ name: pageName})
             }
         }
 }
@@ -19,20 +52,64 @@ export default {
 </script>
 
 <style scoped>
-#topbar {
-    height: 50px;
-    background: rgb(34, 8, 185);
+
+.topbar {
     position: fixed;
     top: 0;
+    bottom: 0;
     left: 0;
-    right: 0;
+    height: 15%;
+    width: 100%;
+    background: rgb(34, 8, 185);
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    padding-top: 1%;
 }
+
+
+
+.topbarButton {
+    background-color: rgb(0, 60, 255);
+    width: 100px;
+    height: 100px;
+    border-radius: 10px;
+
+}
+
+
+.topbarButton:hover{
+    background: rgb(106, 121, 187);
+}
+
+.topbarButton:active{
+    background: rgb(1, 1, 150);
+}
+
+
+
+.icon {
+    padding: 30px;
+}
+
 #settings {
     float: right;
 }
 h2 {
-    text-align: center;
+    text-align: left;
 }
+
+#homeIcon {
+    background: url('@/assets/Home.svg') no-repeat center;
+}
+#userIcon {
+    background: url('@/assets/User.svg') no-repeat center;
+}
+#settingsIcon {
+    background: url('@/assets/Settings.svg') no-repeat center;
+}
+
+
 
 
 </style>
