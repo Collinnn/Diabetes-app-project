6ac67ee7a392ad2be33d2093defd2b4012e8f9ba
Agentii <56613955+Agentii@users.noreply.github.com>
2022-04-01 09:27:43 +0200

    Initial settings menu/window
    Includes slightly transparent window with four non-functioning buttons

--- Diabetes-app-project/src/components/Topbar.vue
+++ Diabetes-app-project/src/components/Topbar.vue
@@ -9,6 +9,12 @@
         <button class = "topbarButton" id = "settingsButton">
             <svg class="icon" id="settingsIcon"></svg>
         </button>
+        <div id="settingsMenu">
+            <button class="settingsButton"> Settings 1 </button>
+            <button class="settingsButton"> Settings 2 </button>
+            <button class="settingsButton"> Settings 3 </button>
+            <button class="settingsButton"> Settings 4 </button>
+        </div>
     </div>
 </template>
 
@@ -51,7 +57,7 @@
 .topbar {
     position: fixed;
     top: 0;
-    bottom: 0;
+    right: 0;
     left: 0;
     height: 15%;
     width: 100%;
@@ -64,8 +70,6 @@
     padding-top: 1%;
 }
 
-
-
 .topbarButton {
     cursor: pointer;
     background-color: rgb(0, 60, 255);
@@ -76,7 +80,6 @@
     border: none;
 }
 
-
 .topbarButton:hover{
     background: rgb(106, 121, 187);
 }
@@ -85,10 +88,14 @@
     background: rgb(1, 1, 150);
 }
 
-
 .icon {
     width: 75px;
     height: 75px;
+}
+
+.settingsButton {
+    margin: 0px 10px;
+    padding: 10px 0px;
 }
 
 #homeIcon {
@@ -100,8 +107,21 @@
 #settingsIcon {
     background: url('@/assets/Settings.svg') no-repeat center;
 }
-
-
+#settings {
+    position: relative;
+}
+#settingsMenu {
+    position: absolute;
+    border-radius: 10px;
+    right: calc((100% - 300px)/6);
+    top: 85%;
+    width: 200px;
+    height: 300px;
+    background-color: rgba(135, 135, 135, 0.98);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
 
 
 </style>
