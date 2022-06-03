b61b239e8165c7f39a9f6f44ecb0b3447b8b587d
Collinnn <tobias3060@gmail.com>
2022-06-03 11:16:54 +0200

    Removed old responses and introduced new delete response

--- Backend/Diabietes-Manager/src/main/java/dtu/controllers/PatientController.java
+++ Backend/Diabietes-Manager/src/main/java/dtu/controllers/PatientController.java
@@ -3,7 +3,6 @@ package dtu.controllers;
 import java.util.List;
 import java.util.Optional;
 
-import javax.annotation.processing.Generated;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +10,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import dtu.model.Doctor;
 import dtu.model.Patient;
 import dtu.repositories.DoctorRepository;
@@ -60,6 +57,12 @@ public class PatientController {
 		return ResponseEntity.ok(patientRepository.save(oPatient.get()));
 	}
 
+	@DeleteMapping("/patients/{patientId}")
+	public ResponseEntity<?> deletePatientById(@PathVariable int patientId){
+		patientRepository.deleteById(patientId);
+		return ResponseEntity.noContent().build();
+	}
+
 	// Gets all Patients associated with a doctor
 	@GetMapping("/doctors/{doctorId}/patients")
 	public ResponseEntity<List<Patient>> getAllDoctors(@PathVariable int doctorId) {
@@ -70,49 +73,4 @@ public class PatientController {
 		return ResponseEntity.ok(doctor.get().getPatients());
 	}
 	
-	// Post new patient to repository
-	@PostMapping("/doctors/{doctorId}/patients")
-	public ResponseEntity<Patient> create(@RequestBody Patient patient, @PathVariable int doctorId) {
-		Optional<Doctor> doctor = doctorRepository.findById(doctorId);
-		if (doctor.isEmpty()) {
-			return ResponseEntity.notFound().build();
-		}
-		doctor.get().addPatient(patient);
-		return ResponseEntity.ok(patientRepository.save(patient));
-	}
-	
-	@PutMapping("/doctors/{doctorId}/patients/{patientId}")
-	public ResponseEntity<Patient> update(@PathVariable int doctorId, @PathVariable int patientId, @RequestBody Patient patient){
-		Optional<Doctor> doctor = doctorRepository.findById(doctorId);
-		if(doctor.isEmpty()) {
-			return ResponseEntity.notFound().build();
-		}
-		Optional<Patient> oPatient = patientRepository.findById(patientId);
-		if(oPatient.isEmpty()) {
-			return ResponseEntity.notFound().build();
-		}
-		oPatient.get().setFirstName(patient.getFirstName());
-		oPatient.get().setLastName(patient.getLastName());
-		oPatient.get().setPassword(patient.getPassword());
-		oPatient.get().setDateOfBirth(patient.getDateOfBirth());
-		return ResponseEntity.ok(patientRepository.save(oPatient.get()));
-	}
-	
-
-	
-	
-	@DeleteMapping("/doctors/{doctorId}/patients/{patientId}")
-	public ResponseEntity<?> delete(@PathVariable int doctorId, @PathVariable int patientId) {
-		Optional<Doctor> doctor = doctorRepository.findById(doctorId);
-		if (doctor.isEmpty()) {
-			return ResponseEntity.notFound().build();
-		}
-		Optional<Patient> patient = patientRepository.findById(patientId);
-		if (patient.isEmpty()) {
-			return ResponseEntity.notFound().build();
-		}
-		doctor.get().removePatient(patient.get());
-		doctorRepository.save(doctor.get());
-		return ResponseEntity.noContent().build();
-	}
 }
