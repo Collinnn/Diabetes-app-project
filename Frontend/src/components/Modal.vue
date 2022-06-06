993bc75f667927d98f4005b5fd99f36fc5bb3ea3
Agentii <56613955+Agentii@users.noreply.github.com>
2022-06-06 12:59:02 +0200

    Added quick styling to modal

--- Frontend/src/components/Modal.vue
+++ Frontend/src/components/Modal.vue
@@ -1,13 +1,17 @@
 <template>
-    <div id="modalMask" @click="$emit("close")">
+    <div id="modalMask" @click="$emit('close')">
         <div id="modal">
-            <slot name="heading">
-                <h2>❗❗❗WARNING❗❗❗ </h2>
-            </slot>
-            <slot name="body">
-                Eat something or you're gonna feel bad, stupid... 
-            </slot>
-            <button @click="$emit("close")"> Ok </button>
+            <div id="modalHead">
+                <slot name="heading">
+                    <h1>❗❗❗WARNING❗❗❗ </h1>
+                </slot>
+            </div>
+            <div id="modalBody">
+                <slot name="body">
+                    Eat something or you're gonna feel bad, stupid... 
+                </slot>
+            </div>
+            <button id="acceptButton" @click="$emit('close')"> Ok </button>
         </div>
     </div>
 
@@ -23,12 +27,32 @@
 
 <style>
 #modalMask {
-    background-color: rgba(0, 0, 0, 0.4);
+    background-color: rgba(0, 0, 0, 0.6);
     position: fixed;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
+}
+#modal {
+    margin: 60px auto;
+    width: 350px;
+    height: auto;
+    background-color: rgb(200, 200, 200);
+    border-radius: 3px;
+    padding: 5px 10px;
+}
+#modalHead {
+    min-height: 40px;
+}
+#modalBody {
+    min-height: 60px;
+}
+#acceptButton {
+    position: relative;
+    bottom: 20px;
+    float: right;
+
 }
 
 </style>
--- Frontend/src/views/overview.vue
+++ Frontend/src/views/overview.vue
@@ -1,17 +1,19 @@
 <template>
     <div>
         <h1>Overview Page</h1>
-        <DoctorForm />       
+        <DoctorForm />
+        <ModalBox />   
     </div>    
 </template>
 
 
 <script>
 import DoctorForm from "@/components/DoctorForm.vue";
+import ModalBox from "@/components/Modal.vue"
 
 export default {
     name: "overviewPage",
-    components: { DoctorForm }
+    components: { DoctorForm, ModalBox }
 }
 
 </script>
