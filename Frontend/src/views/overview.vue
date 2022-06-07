00c3036e441326c5e40ad2b9ce6fbf19c5fb9c6c
Agentii <56613955+Agentii@users.noreply.github.com>
2022-06-07 09:30:32 +0200

    Removed doctor form

--- Frontend/src/views/overview.vue
+++ Frontend/src/views/overview.vue
@@ -1,17 +1,14 @@
 <template>
     <div>
         <h1>Overview Page</h1>
-        <DoctorForm />       
     </div>    
 </template>
 
 
 <script>
-import DoctorForm from "@/components/DoctorForm.vue";
 
 export default {
-    name: "overviewPage",
-    components: { DoctorForm }
+    name: "overviewPage"
 }
 
 </script>
