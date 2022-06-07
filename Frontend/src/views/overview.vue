695a47f063f8bf529e0dda7ae38279f7f1b61d1c
Agentii <56613955+Agentii@users.noreply.github.com>
2022-06-07 13:14:48 +0200

    Simple DoughnutChart

--- Frontend/src/views/overview.vue
+++ Frontend/src/views/overview.vue
@@ -1,14 +1,32 @@
 <template>
     <div>
         <h1>Overview Page</h1>
+        <DoughnutChart ref="donut" :chart-data="chartData"/>
     </div>    
 </template>
 
 
 <script>
 
+import { DoughnutChart } from "vue-chart-3"
+import { Chart, registerables } from 'chart.js'
+
+Chart.register(...registerables)
+
 export default {
-    name: "overviewPage"
+    name: "overviewPage",
+    components: { DoughnutChart },
+    data() {
+        return {
+            chartData: {
+                labels: ["Jacob", "Simon", "Tobias", "Zwinge"],
+                datasets: [{
+                    data: [50, 40, 30, 20],
+                    backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4']
+                }]
+            },
+        }
+    }
 }
 
 </script>
