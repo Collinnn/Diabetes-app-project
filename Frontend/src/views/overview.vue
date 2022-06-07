13f36757bd0d368ce300b5ec80628eb9ed6ade4b
Agentii <56613955+Agentii@users.noreply.github.com>
2022-06-07 09:53:55 +0200

    Squashed commit of the following:
    commit 00c3036e441326c5e40ad2b9ce6fbf19c5fb9c6c
    Author: Agentii <56613955+Agentii@users.noreply.github.com>
    Date:   Tue Jun 7 09:30:32 2022 +0200
    
        Removed doctor form

--- Frontend/src/views/overview.vue
+++ Frontend/src/views/overview.vue
@@ -1,20 +1,18 @@
 <template>
     <div>
         <h1>Overview Page</h1>
-        <DoctorForm />
         <ModalBox v-if="this.isModalVisible" @close="this.isModalVisible = false" > </ModalBox>
-        <button @click="this.isModalVisible = true"> Click to open modal </button> 
+        <button @click="this.isModalVisible = true"> Click to open modal </button>
     </div>    
 </template>
 
 
 <script>
-import DoctorForm from "@/components/DoctorForm.vue";
 import ModalBox from "@/components/Modal.vue"
 
 export default {
     name: "overviewPage",
-    components: { DoctorForm, ModalBox },
+    components: { ModalBox },
     data() {
         return {
             isModalVisible: false
