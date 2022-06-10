a8545506a6874a43340614a602df6a56472f3376
Agentii <56613955+Agentii@users.noreply.github.com>
2022-06-10 16:48:58 +0200

    Empty admin page

---
+++ Frontend/src/views/admin.vue
@@ -0,0 +1,20 @@
+<template>
+    <title> Admin page </title>
+    <div class="container">
+        <Sidebar />
+    </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar.vue';
+
+export default {
+    name: "adminPage",
+    components: { Sidebar }
+}
+
+</script>
+
+<style>
+
+</style>
