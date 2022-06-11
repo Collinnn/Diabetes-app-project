16e2410a16d27482c97853ed1b803a5cd681fd7f
Collinnn <51782585+Collinnn@users.noreply.github.com>
2022-06-11 21:32:50 +0200

    Succesfull posting

--- Backend/Diabietes-Manager/src/main/java/dtu/components/PulseMeasurement.java
+++ Backend/Diabietes-Manager/src/main/java/dtu/components/PulseMeasurement.java
@@ -1,14 +1,15 @@
 package dtu.components;
 
+import java.sql.Timestamp;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import dtu.model.Measurement;
-import dtu.model.Patient;
+import dtu.model.MeasurementId;
 
-//import dtu.model.Measurement;
 
 @Component
 public class PulseMeasurement {
@@ -18,15 +19,25 @@ public class PulseMeasurement {
 
 	@Scheduled(cron = "0 */1 * * * *")
 	public void reportCurrentTime() {
-		int patientId = 2;
+		int patientId = 1;
 		String url = "http://localhost:8080/api/v1/patients/" + patientId + "/measurements";
 			
 		
 		RestTemplate restTemplate = new RestTemplate();
 		HttpEntity<Measurement> request = new HttpEntity<>(new Measurement());
-		Measurement measurement = new Measurement();
-		measurement.setGlucoseLevel(2.0);
-		restTemplate.postForObject(url, request, Measurement.class,measurement);
+		System.out.println(request.getBody());
+		
+		//NEEDED TO MAKE A TIMESTAMP
+		Timestamp ts = new Timestamp(System.currentTimeMillis());
+		MeasurementId measurementId = new MeasurementId();
+		measurementId.setPatientId(patientId);
+		measurementId.setTimestamp(ts);
+		/*END OF TIMESTAMP CREATION */
+		
+		request.getBody().setMeasurementId(measurementId);
+
+		
+		restTemplate.postForObject(url, request, Measurement.class);
 		
 		
 		System.out.println("Hello world");
