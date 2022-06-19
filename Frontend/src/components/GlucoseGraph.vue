89b55b605ddc07d6ecb39acce16e0d3941537655
Agentii <56613955+Agentii@users.noreply.github.com>
2022-06-19 20:22:53 +0200

    Refactor graph page

--- Frontend/src/components/GlucoseGraph.vue
+++ Frontend/src/components/GlucoseGraph.vue
@@ -1,136 +1,298 @@
 <template>
-<div>
-    <h1>HELLO WORLD</h1>
-    <canvas id="glucoseChart" width="400" height="400"></canvas>
-</div>
+  <Line
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :dataset-id-key="datasetIdKey"
+    :plugins="plugins"
+    :css-classes="cssClasses"
+    :styles="styles"
+    :width="width"
+    :height="height"
+  />
 </template>
 
-
 <script>
-    import {Line} from 'vue-chartjs'
-    import { Chart,registerables } from 'chart.js'
+import { Line } from 'vue-chartjs'
+import { Chart as ChartJS, registerables} from 'chart.js'
 
-    Chart.register(...registerables)
-    export default {
-        name: 'GlucoseGraph',
-        componets: {Line},
-        data(){
-            return{
-                glucoseArray: [],
-                timestampArray:[],
-                timer:null
-            }
+ChartJS.register(...registerables)
+
+
+export default {
+  name: 'GraphChart',
+  components: { Line },
+  emits: ["lowBloodSugar", "highBloodSugar"],
+  data() {
+    return {
+      totalDataPoints: 0,
+      sampleGlucoseLevels: [],
+      sampleTimestamps: [],
+      glucoseLevelsPerDailyTimestamp: [], // Array of arrays with glucose levels for each minute in measurements data
+      lowerConfidenceLevels: [],
+      upperConfidenceLevels: [],
+      
+      lowGlucoseLevel: 4,
+      highGlucoseLevel: 7,
+      dataTimeSpan: 1*1*60*60*1000,  // *60 minutes* in milliseconds
+      timer: null,
+
+      chartOptions: {
+        responsive: true,
+        scales: {
+          y: {
+            min: 0,
+            ticks:{
+                  color: document.getElementById('app').style.getPropertyValue("--text-color") // watch changes
+              },
+              title: {
+                display: true,
+                text: "Glucose level [mmol/L]",
+                color: document.getElementById('app').style.getPropertyValue("--text-color")
+              }
+          },
+          x:{
+              ticks:{
+                  color: document.getElementById('app').style.getPropertyValue("--text-color")
+              },
+              title: {
+                display: true,
+                text: "Time of the hour [min]",
+                color: document.getElementById('app').style.getPropertyValue("--text-color")
+              }
+              
+          }
         },
-        async mounted(){
-            console.log('We got it mounted boys')
-           
-            await this.axios.get(this.$backend.getUrlGetMeasurementsFromPatientById(1))
-            .then(response =>{
-                let tmpDate;
-                let today = new Date()
-                for (let i = 0; i < response.data.length; i++) {
-                    tmpDate = response.data[response.data.length-1-i].measurementId.timestamp;
-                    if(today.getTime() - 2*24*60*60*1000 > this.dateTimeHandling(tmpDate).getTime()){
-                    break;
-                    }
-                    this.timestampArray[i] = this.dateToString(this.dateTimeHandling(tmpDate));
-                    this.glucoseArray[i] = response.data[i].glucoseLevel;
-                }
-                    this.reverseArrays();
-                console.log("end of initial graph");
-            }).catch((error) => console.log(error));
-            
-
-            const ctx = document.getElementById('glucoseChart');
-            const glucoseChart = new Chart(ctx,{
-                type: 'line',
-                data: {
-                    labels: this.timestampArray,
-                    datasets: [{
-                        label: 'Last 24 hours',
-                        data: this.glucoseArray,
-                        borderColor: ['#6295D9'],
-                        tension: 0.1
-                    }]
-                },
-                options: {
-                    plugins:{
-                        legend:{
-                            labels:{
-                                color: document.getElementById('app').style.getPropertyValue("--text-color")
-                            }
-                        }
-                    },
-                    scales: {
-                        y: {
-                            beginAtZero: true,
-                             color: "purple"
-                        },
-                        x:{
-                            ticks:{
-                                color: document.getElementById('app').style.getPropertyValue("--text-color")
-                            }
-                            
-                        }
-                    }
-                }
-            });
-            glucoseChart; 
-            this.timer = setInterval(async()=>{
-                this.reverseArrays();
-                await this.axios.get(this.$backend.getUrlGetMeasurementsFromPatientById(1))
-                .then(response =>{
-
-                    
-
-                   
-                    const tmpDate = response.data[response.data.length-1].measurementId.timestamp;
-                    const tmpdata = response.data[response.data.length-1].glucoseLevel;
-
-                   
-                }).catch((error) => console.log(error));
-                
-                this.glucoseArray[this.glucoseArray.length] =tmpdata;
-                this.timestampArray.push(this.dateToString(this.dateTimeHandling(tmpDate)));
-                this.reverseArrays();
-                
-            },10000) //Every 10 sec     
-
-        },
-        methods:{
-            dateTimeHandling: function(tmpDate){
-                let dateTime = new Date();
-                const [date, time] = tmpDate.split(' ');
-                const [year, month, day] = date.split('-');
-                const [hour, minute, second] = time.split(':')
-                dateTime.setUTCFullYear(year);
-                dateTime.setUTCMonth((month-1));
-                dateTime.setUTCDate(day);
-                dateTime.setUTCHours(hour);
-                dateTime.setUTCMinutes(minute);
-                dateTime.setUTCSeconds(second);
-                return dateTime;
-            },
-            dateToString: function(date){
-                let tmp;
-                tmp = date.toISOString().split('T');
-                tmp = tmp[1].split('.');
-                return tmp[0]
-            },reverseArrays: function(){
-                this.glucoseArray.reverse();
-                this.timestampArray.reverse();
+        plugins: {
+          title: {
+            display: true,
+            text: "Glucose levels",
+            color: document.getElementById('app').style.getPropertyValue("--text-color"),
+            font: {
+              family: "'arial'",
+              size: 30,
+              weight: "bold"
             }
+          },
+          legend: {
+            display: false
+          },
         }
-    };
+      },
+      chartId: 'glucose-chart',
+      datasetIdKey: 'label',
+      width: 1200,
+      height: 800,
+      cssClasses: 'graphContainer',
+      styles: {},
+      plugins: []
+    }
+  },
+  async mounted() {
+    await this.loadData()
+    this.timer = setInterval(async () => {
+      this.updateData()
+    }, 60000) // Every 60 sec
+  },
+  beforeUnmount() {
+    clearInterval(this.timer)
+  },
+  methods: {
+    async loadData() {
+      let data = await this.fetchData()
+      this.totalDataPoints = data.length
+      await this.loadGraphData(data)
+      console.log(this.sampleGlucoseLevels)
+      await this.loadGlucoseLevelsPerDailyTimestamp(data)
+      await this.loadConfidenceIntervals()
+    },
+    async fetchData() { // REMEMBER TO FETCH FOR RIGHT PATIENT
+      let data;
+      await this.axios.get(this.$backend.getUrlGetMeasurementsFromPatientById(3))
+       .then( response => {
+          data = response.data
+       })
+      return data
+    },
+    async loadGraphData(data) { // RENAME
+      let tmpDate, sampleTimestamp;
+      let today = new Date()
+      for (let i = 0; i < data.length; i++) {
+          tmpDate = data[data.length-1-i].measurementId.timestamp;
+          if ((today.getTime() - this.dataTimeSpan) > this.dateTimeHandling(tmpDate).getTime()) {
+              break
+          }
+          sampleTimestamp = this.dateToString(this.dateTimeHandling(tmpDate))
+          this.sampleTimestamps[i] = sampleTimestamp
+          this.sampleGlucoseLevels[sampleTimestamp] = data[data.length-1-i].glucoseLevel
+      }
+      this.sampleTimestamps.reverse()
+    },
+    async loadGlucoseLevelsPerDailyTimestamp(data) {
+      let timestamp;
+      this.glucoseLevelsPerDailyTimestamp = data.reduce( (res, curr) => {
+        timestamp = this.dateToString(this.dateTimeHandling(curr.measurementId.timestamp))
+        if (res[timestamp] === undefined) {
+          res[timestamp] = [curr.glucoseLevel]
+        } else {
+          res[timestamp].push(curr.glucoseLevel)
+        }
+        return res
+      }, [])
+    },
+    async loadConfidenceIntervals() {
+      let sampleConfidenceInterval;
+      Object.entries(this.glucoseLevelsPerDailyTimestamp).forEach( ([timestamp, sample]) => {
+        sampleConfidenceInterval = this.getSampleConfidenceInterval(sample)
+        this.lowerConfidenceLevels[timestamp] = sampleConfidenceInterval.lowerBound
+        this.upperConfidenceLevels[timestamp] = sampleConfidenceInterval.upperBound
+      })
+    },
+    getSampleConfidenceInterval(sample) {
+      let sampleSize, sampleMean, sampleVariance, sampleStandardDeviation, meanDeviation;
+      let confidenceInterval = { lowerBound: 0, upperBound: 0 }
+      sampleSize = sample.length
+      sampleMean = sample.reduce((res, curr) => res + curr, 0) / sampleSize
+      if (sampleSize == 1) {
+        confidenceInterval = { lowerBound: sampleMean, upperBound: sampleMean}
+      } else {
+        sampleVariance = sample.reduce((res, curr) => res + (curr - sampleMean)**2, 0) / (sampleSize - 1)
+        sampleStandardDeviation = Math.sqrt(sampleVariance)
+        meanDeviation = 1.96 * sampleStandardDeviation / Math.sqrt(sampleSize)
+        confidenceInterval.lowerBound = sampleMean - meanDeviation
+        confidenceInterval.upperBound = sampleMean + meanDeviation
+      }
+      return confidenceInterval
+    },
+
+    async updateData() { 
+      let data = await this.fetchData()
+      await this.loadGraphData(data)
+      if (data.length > this.totalDataPoints) {
+        let newDataPoints = data.slice(this.totalDataPoints)
+        newDataPoints.forEach((dataPoint) => {
+          if (dataPoint.glucoseLevel < this.lowGlucoseLevel) {
+            this.$emit("lowBloodSugar", dataPoint.glucoseLevel.toFixed(2))
+          } 
+          else if (dataPoint.glucoseLevel > this.highGlucoseLevel) {
+            this.$emit("highBloodSugar", dataPoint.glucoseLevel.toFixed(2))
+          }
+        })
+        await this.updateGlucoseLevelsPerDailyTimestamp(newDataPoints)
+        await this.updateConfidenceIntervals(newDataPoints)
+        this.totalDataPoints = data.length
+      }
+    },
+    async updateGlucoseLevelsPerDailyTimestamp(data) {
+      let timestamp, currDataPoint;
+        for (let i = 0; i < data.length; i++) {
+          currDataPoint = data[data.length-1-i]
+          timestamp = this.dateToString(this.dateTimeHandling(currDataPoint.measurementId.timestamp))
+          if (this.glucoseLevelsPerDailyTimestamp[timestamp] === undefined) {
+            this.glucoseLevelsPerDailyTimestamp[timestamp] = [currDataPoint.glucoseLevel]
+          } else {
+            this.glucoseLevelsPerDailyTimestamp[timestamp].push(currDataPoint.glucoseLevel)
+          }
+        }
+    },
+    async updateConfidenceIntervals(data) {
+      let timestamp, sample, confidenceInterval;
+      data.forEach((currDataPoint) => {
+        timestamp = this.dateToString(this.dateTimeHandling(currDataPoint.measurementId.timestamp))
+        sample = this.glucoseLevelsPerDailyTimestamp[timestamp]
+        confidenceInterval = this.getSampleConfidenceInterval(sample)
+        this.lowerConfidenceLevels[timestamp] = confidenceInterval.lowerBound
+        this.upperConfidenceLevels[timestamp] = confidenceInterval.upperBound
+      })
+    },
+
+    dateTimeHandling(tmpDate) {
+      let dateTime = new Date();
+      const [date, time] = tmpDate.split(' ');
+      const [year, month, day] = date.split('-');
+      const [hour, minute, second] = time.split(':')
+      dateTime.setFullYear(year);
+      dateTime.setMonth((month-1));
+      dateTime.setDate(day);
+      dateTime.setHours(hour, minute, second);
+      return dateTime;
+    },
+    dateToString(date) {
+      let tmp;
+      tmp = date.toISOString().split('T');
+      tmp = tmp[1].split('.');
+      return tmp[0].substring(3, 5)
+    }
+  },
+  computed: {
+    chartData() {
+      return {
+        labels: this.sampleTimestamps,
+        datasets: [
+          {
+            label: "Lower bound for healthy level",
+            data: this.sampleTimestamps.map(() => this.lowGlucoseLevel),
+            borderColor: 'rgba(0, 255, 0, 0.5)',
+            pointRadius: 0,
+            pointHitRadius: 0
+          },
+          {
+            label: "Upper bound for healthy level",
+            data: this.sampleTimestamps.map(() => this.highGlucoseLevel),
+            borderColor: 'rgba(0, 255, 0, 0.5)',
+            pointRadius: 0,
+            pointHitRadius: 0
+
+          },
+          { 
+            label: "Lower confidence levels",
+            fill: false,
+            data: this.sampleTimestamps.map((ts) => this.lowerConfidenceLevels[ts]), 
+            backgroundColor: 'rgba(255, 0, 0, 0.2)',
+            borderColor: 'rgba(0, 255, 0, 0.2)',
+            pointRadius: 0,
+            pointHitRadius: 0,
+            lineTension: 0.2
+          },
+          { 
+            label: 'Glucose levels',
+            fill: {
+              target: 2,
+              below: 'transparent',
+              above: 'rgba(0, 255, 0, 0.2)'
+            },
+            data: this.sampleTimestamps.map((ts) => this.sampleGlucoseLevels[ts]), 
+            backgroundColor: 'rgb(0, 0, 255)',
+            borderColor: 'rgb(75, 192, 192)',
+          },
+          { 
+            label: "Upper confidence levels",
+            fill: {
+              target: 3,
+              below: 'transparent',
+              above: 'rgba(255, 0, 0, 0.2)'
+            },
+            data: this.sampleTimestamps.map((ts) => this.upperConfidenceLevels[ts]), 
+            backgroundColor: 'rgba(255, 0, 0, 0.2)',
+            borderColor: 'rgba(0, 255, 0, 0.2)',
+            pointRadius: 0,
+            pointHitRadius: 0,
+            lineTension: 0.2
+          }, 
+        ]
+      }
+    }
+  }
+  
+}
 </script>
 
-
-
-
-
-<style scoped>
-
-
-
+<style>
+.graphContainer {
+  position: relative;
+  width: 60%;
+  height: 50%;
+  background-color: var(--secondary-color);
+}
 
 </style>
--- Frontend/src/views/patient/graphPage.vue
+++ Frontend/src/views/patient/graphPage.vue
@@ -2,15 +2,55 @@
     <title>Graph page</title>
     <div>
         <h1>Graph Page</h1>
+        <ModalBox v-if="isModalVisible" @close="isModalVisible = false" >
+            <template #heading>
+                <h1> {{modalHeading}} </h1>
+            </template>
+            <template #body>
+                <p> {{modalBody}} </p>
+            </template>
+        </ModalBox>
+        <div class="graph">
+            <GlucoseGraph @low-blood-sugar="showLowBloodSugarMessage" @high-blood-sugar="showHighBloodSugarMessage"/>
+        </div>
     </div>    
 </template>
 
+
 <script>
+import GlucoseGraph from '@/components/GlucoseGraph.vue';
+import ModalBox from "@/components/Modal.vue"
 
 export default {
-    name: "graphPage"
+    name: "graphPage",
+    components: { GlucoseGraph, ModalBox },
+    data() {
+        return {
+            isModalVisible: false,
+            modalHeading: "",
+            modalBody: ""
+        }
+    },
+    methods: {
+        showLowBloodSugarMessage(glucoseLevel) {
+            this.isModalVisible = true
+            this.modalHeading = "❗Alert: Low blood sugar❗"
+            this.modalBody = "Your blood sugar has reached a low level of " + glucoseLevel + " mmol/L"
+        },
+        showHighBloodSugarMessage(glucoseLevel) {
+            this.isModalVisible = true
+            this.modalHeading = "❗Alert: High blood sugar❗"
+            this.modalBody = "Your blood sugar has reached a high level of " + glucoseLevel + " mmol/L"
+
+        }
+    }
 }
+
 </script>
 
 <style>
+.graph {
+    display: flex;
+    justify-content: center;
+}
 </style>
